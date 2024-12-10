3bc5bd788a16b00f882724f401216e781e16370f
ekhassan <ek.hassan.07@gmail.com>
2024-12-11 00:07:13 +0500

    Not Found Page Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Signup from "./pages/Signup"
 import AddRecipe from "./pages/AddRecipe"
 import EditRecipe from "./pages/EditRecipe"
 import RecipeFeed from "./pages/RecipeFeed"
+import NotFound from "./pages/NotFound"
 
 
 const App = () => {
@@ -20,6 +21,7 @@ const App = () => {
         <Route path="/detail/:id" element={<DetailPage />} />
         <Route path="/add-recipe" element={<AddRecipe />} />
         <Route path="/edit-recipe/:id" element={<EditRecipe />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   )
