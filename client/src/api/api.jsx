ba080da7aace784700b7530bff2554eabf746dbb
ekhassan <ek.hassan.07@gmail.com>
2024-12-29 01:14:04 +0500

    Add authorization token to API headers for authenticated requests

--- client/src/api/api.jsx
+++ client/src/api/api.jsx
@@ -1,8 +1,12 @@
 import axios from 'axios'
+import useAuthStore from '../store/useAuthStore'
+
+const { token } = useAuthStore();
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
     headers: {
+        "Authorization": `Bearer ${token}`,
         'Content-Type': 'application/json'
     },
     withCredentials: true,
